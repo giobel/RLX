ce77f92dda97da311631e37c4c663d8e20df15f5
Gio <giobro@gmail.com>
2024-09-02 18:24:19 +0900

    update

---
+++ RLX/CopyParams.cs
@@ -0,0 +1,69 @@
+#region Namespaces
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+#endregion
+
+namespace RLX
+{
+    [Transaction(TransactionMode.Manual)]
+    public class CopyParams : IExternalCommand
+    {
+        public Result Execute(
+          ExternalCommandData commandData,
+          ref string message,
+          ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+
+            Reference sourcePiperef = uidoc.Selection.PickObject(ObjectType.Element, "Select source");
+
+            Element sourcePipe = doc.GetElement(sourcePiperef);
+
+            IList<Reference> destinationPipesRef = uidoc.Selection.PickObjects(ObjectType.Element, "Select destination pipes");
+
+                List<string> paramsToSet = new List<string>(){"RLX_ActualCost",
+                    "RLX_ClassificationUniclassEF_Description","RLX_Title",
+                "RLX_ClassificationUniclassEF_Number","RLX_ClassificationUniclassPr_Description",
+                "RLX_ClassificationUniclassPr_Number","RLX_ClassificationUniclassSs_Description",
+                "RLX_ClassificationUniclassSs_Number","RLX_Component","RLX_CoordinatesX","RLX_CoordinatesY",
+                "RLX_CoordinatesZ","RLX_Facility","RLX_GridReferenceSystem","RLX_Location","RLX_MaintenanceCost",
+                "RLX_Space","RLX_Specification","RLX_System","RLX_Type","RLX_UniqueIdentifier","RLX_Zone","DS_AssetID"};
+
+                using (Transaction t = new Transaction(doc, "Copy pipe parameters"))
+                {
+                    t.Start();
+
+                    foreach (Reference pipeRef in destinationPipesRef)
+                    {
+
+                           Element pipe = doc.GetElement(pipeRef);
+                            foreach (string paramName in paramsToSet)
+                            {
+                                //					TaskDialog.Show("R", paramName);
+                                string p = sourcePipe.LookupParameter(paramName).AsValueString();
+                                pipe.LookupParameter(paramName).Set(p);
+                            }
+
+                }
+                    t.Commit();
+                }//close transaction
+
+
+            
+
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ RLX/Valves/FillValveParams.cs
@@ -0,0 +1,59 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RLX.Valves
+{
+    internal class FillValveParams
+    {
+        public void FillValvesParams()
+        {
+            UIDocument uidoc = this.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+
+            List<BuiltInCategory> builtInCats = new List<BuiltInCategory>();
+            builtInCats.Add(BuiltInCategory.OST_PipeAccessory);
+
+            ElementMulticategoryFilter filter1 = new ElementMulticategoryFilter(builtInCats);
+
+
+            IList<Element> elements = new FilteredElementCollector(doc, doc.ActiveView.Id).WherePasses(filter1).WhereElementIsNotElementType().ToElements();
+
+
+            using (Transaction t = new Transaction(doc, "Fill valves params"))
+            {
+
+                t.Start();
+
+                foreach (Element element in elements)
+                {
+
+                    Parameter title = element.LookupParameter("RLX_Title");
+
+
+
+                    if (title.AsValueString() == null || title.AsValueString().Length < 3)
+                    {
+
+                        //						TaskDialog.Show("T", title.AsValueString());
+
+                        title.Set(element.LookupParameter("Family").AsValueString());
+
+                    }
+
+
+
+                }
+
+
+                t.Commit();
+            }
+
+        }
+    }
+}
